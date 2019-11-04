e94347145bb0484aaa8c4bab38454a9c8b43c243
Mark Allison <mark@stylingandroid.com>
2019-11-04 19:00:17 +0000

    Initial commit

---
+++ app/src/main/java/com/stylingandroid/serializableobject/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.stylingandroid.serializableobject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
